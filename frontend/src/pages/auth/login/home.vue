c04420283e2b2075584ac565b08cbcd5836cdbd3
Dione Bruno <dionebruno88@gmail.com>
2026-05-16 18:29:09 -0400

    Fix

---
+++ frontend/src/pages/auth/login/home.vue
@@ -0,0 +1,93 @@
+<template>
+  <q-page class="row full-height">
+    <!-- Imagem de fundo -->
+    <div class="absolute-full" style="z-index: -1">
+      <q-img
+        src="https://ministeriofiel.com.br/wp-content/uploads/2023/04/orac%CC%A7a%CC%83o-1.png"
+        fit="cover"
+        style="width: 100%; height: 100%"
+      />
+    </div>
+
+    <!-- Painel de login -->
+    <div class="fit row justify-end full-height align-items-center">
+      <div class="col-12 col-sm-6 col-md-5 col-lg-4 q-pa-md custom-border">
+        <div
+          class="flex justify-center items-center column fit bg-grey-1 full--width rounded-panel q-pa-md shadow-7 card-hover"
+          style="opacity: 0.93; max-width: 400px; width: 100%; padding: 16px; margin: auto"
+        >
+          <div class="row col-11 col-sm-7 col-md-5">
+            <!-- Logo Section -->
+            <div class="column col-11 col-md-7 q-mx-auto">
+              <p class="text-h4 col-12 q-ma-none q-py-md text-center">
+                Kehila
+                <!-- <q-img
+                  v-if="!loginTemplate.logo.title"
+                  :src="eomnesLogo"
+                  :class="loginTemplate.logo.class"
+                  :style="loginTemplate.logo.style"
+                  fit="contain"
+                /> -->
+              </p>
+              <!-- <q-img
+                v-else-if="loginTemplate.logo.type === 'image'"
+                :src="loginTemplate.logo.srcImg ? loginTemplate.logo.srcImg : eomnesLogo"
+                :class="loginTemplate.logo.class"
+                :style="loginTemplate.logo.style"
+                fit="contain"
+              /> -->
+            </div>
+            <div class="col-12">
+              <LoginForm />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive, toRefs } from "vue";
+import LoginForm from "./loginForm.vue";
+
+export default defineComponent({
+  name: "authLoginHome",
+  components: {
+    LoginForm,
+  },
+  setup() {
+    const data = reactive({});
+
+    return {
+      ...toRefs(data),
+    };
+  },
+});
+</script>
+
+<style scoped>
+.full-height {
+  height: 100vh;
+}
+
+.row {
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  /* height: 100vh; */
+}
+
+.q-page {
+  padding-right: 2vw;
+}
+
+.rounded-panel {
+  border-radius: 12px;
+  overflow: hidden;
+}
+
+.shadow-4 {
+  box-shadow: 0 8px 24px rgba(0, 0, 0, 0.15);
+}
+</style>
---
+++ frontend/src/pages/auth/login/loginForm.vue
@@ -0,0 +1,156 @@
+<template>
+  <div class="row">
+    <div class="col-12 q-my-md">
+      <q-separator color="secondary" />
+    </div>
+
+    <div class="col-12 q-my-md"></div>
+    <!-- Username Input -->
+    <div class="col-12 q-my-sm rounded-panel">
+      <q-input
+        filled
+        stack-label
+        unmasked-value
+        autofocus
+        v-model="input.username"
+        autocomplete="off"
+        bg-color="grey-3"
+        data-cy="username"
+        type="text"
+        label="Usuário"
+        hint="CPF ou Email"
+        :mask="`${isCpf(input.username) ? '###.###.###-##' : ''}`"
+        @keyup.enter="inputPassword.focus()"
+      />
+    </div>
+
+    <!-- Password Input -->
+    <div class="col-12 q-my-sm rounded-panel">
+      <q-input
+        filled
+        stack-label
+        ref="inputPassword"
+        autocomplete="off"
+        bg-color="grey-3"
+        v-model="input.password"
+        :type="showPwd ? 'password' : 'text'"
+        data-cy="password"
+        label="Senha"
+        @keyup.enter="login()"
+      >
+        <template v-slot:append>
+          <q-icon
+            :name="showPwd ? 'visibility_off' : 'visibility'"
+            class="cursor-pointer"
+            @click="showPwd = !showPwd"
+          />
+        </template>
+      </q-input>
+    </div>
+
+    <!-- Forgot Password Link -->
+    <div class="col-12 q-my-md">
+      <div class="text-right text-grey-8">
+        <p class="text-subtitle2 cursor-pointer q-ma-none" @click="$emit('recover')">
+          Recuperar conta
+        </p>
+      </div>
+    </div>
+
+    <!-- Login and Digital Certificate Buttons -->
+    <div class="col-12 row justify-between items-center q-mb-xl">
+      <div class="col-9">
+        <q-btn
+          unelevated
+          data-cy="btn-login"
+          label="Entrar"
+          @click="login()"
+          color="primary"
+          class="full-width"
+        />
+      </div>
+      <div class="col-2 text-right">
+        <q-btn flat color="grey-6" icon="developer_board" size="md">
+          <q-tooltip>Acessar com Certificado Digital</q-tooltip>
+        </q-btn>
+      </div>
+    </div>
+
+    <div class="col-12 q-my-md">
+      <q-separator color="secondary" />
+    </div>
+
+    <!-- Social Login Section -->
+    <!-- <div class="col-12" v-if="false">
+      <q-separator color="secondary" />
+      <p class="text-info text-center q-my-md text-subtitle1">Ou faça login com</p>
+      <div class="row justify-center q-gutter-x-md q-mb-xl">
+        <q-btn round flat size="sm" class="bg-grey-3">
+          <q-img :src="facebookIcon" style="width: 24px" />
+        </q-btn>
+        <q-btn round flat size="sm" class="bg-grey-3">
+          <q-img :src="xIcon" style="width: 24px" />
+        </q-btn>
+        <q-btn round flat size="sm" class="bg-grey-3">
+          <q-img :src="googleIcon" style="width: 24px" />
+        </q-btn>
+      </div>
+    </div> -->
+
+    <!-- Sign Up Section -->
+    <div class="col-12 text-center q-mt-md">
+      <p class="q-ma-none text-subtitle1 text-grey-8">
+        Não tem conta?
+        <q-btn flat color="primary" label="Cadastrar" class="q-px-sm" @click="$emit('register')" />
+      </p>
+    </div>
+    <span
+      v-if="$q.platform.is.electron"
+      class="q-pt-md cursor-pointer text-negative"
+      @click="clearLocalStorage()"
+    >
+      <q-icon name="settings" class="" /> Limpar configurações
+    </span>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive, ref, toRefs } from "vue";
+
+export default defineComponent({
+  name: "authLoginLoginForm",
+  setup() {
+    const data = reactive({
+      inputPassword: ref(),
+      // loginTemplate: computed(() => $setting.$state.template.login),
+      showPwd: ref(true),
+      authenticated: ref(false),
+      input: ref({
+        username: "",
+        password: "",
+        companyUuid: "",
+      }),
+    });
+
+    function isCpf(cpf: string): boolean {
+      return cpf.replace(/[^0-9]/g, "").length == 11;
+    }
+
+    function clearLocalStorage() {
+      localStorage.clear();
+      window.location.reload();
+    }
+
+    async function login() {}
+
+    return {
+      ...toRefs(data),
+      login,
+      clearLocalStorage,
+      isCpf,
+    };
+  },
+});
+</script>
+
+<style></style>
