65baaa33814b53c7d97f841179055742d9469961
Dione Bruno <dionebruno88@gmail.com>
2026-05-16 22:32:17 -0400

    Fixz

--- frontend/src/pages/home.vue
+++ frontend/src/pages/home.vue
@@ -1,5 +1,21 @@
 <template>
-  <q-page class="row items-center justify-evenly"> </q-page>
+  <q-page padding>
+    <div class="row q-col-gutter-md">
+      <div class="col-12 col-sm-4 col-md-2 cursor-pointer">
+        <q-card>
+          <q-card-section>
+            <div class="text-h6">Home</div>
+          </q-card-section>
+          <q-card-section>
+            <div class="text-subtitle1">Home</div>
+          </q-card-section>
+          <q-card-section>
+            <div class="text-body1">Home</div>
+          </q-card-section>
+        </q-card>
+      </div>
+    </div>
+  </q-page>
 </template>
 
 <script lang="ts">
